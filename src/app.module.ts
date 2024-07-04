cd62a4c225219997060fa8339092d087ba45ba30
Yassine Azedine <yassine.azedine0711@gmail.com>
2024-07-04 18:05:05 +0100

    third commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
