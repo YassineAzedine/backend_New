84b8a0ba9c48b3112ced68dfb2e071eebc8a7025
Yassine Azedine <yassine.azedine0711@gmail.com>
2024-07-05 16:11:13 +0100

    config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/database11'),CategoryModule, ProductsModule],
+  imports: [MongooseModule.forRoot("mongodb+srv://webw23346:yassine0711@cluster0.yyfuyex.mongodb.net/DB838?retryWrites=true&w=majority&appName=Cluster0"),CategoryModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
